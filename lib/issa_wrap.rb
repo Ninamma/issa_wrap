d6b1d40453d5115ad43312ca618aae1f0c55c1bf
Ninamma Rai <ninammarai@Ninammas-MacBook-Pro.local>
2019-09-03 16:35:36 +0100

    Did the TDD bit

--- lib/issa_wrap.rb
+++ lib/issa_wrap.rb
@@ -2,5 +2,15 @@ require "issa_wrap/version"
 
 module IssaWrap
   class Error < StandardError; end
-  # Your code goes here...
+  class Wrapper
+    def self.wrap(string, column)
+      return "" if string.nil? || string.empty?
+      return string if string.length <= column
+
+      where_to_break = string[0...column].index(" ") || column
+      wrapped = string[0...where_to_break]
+      remainder = string[where_to_break..-1] || ""
+      (wrapped + "\n" + wrap(remainder.strip, column)).strip
+    end
+  end
 end
--- spec/issa_wrap_spec.rb
+++ spec/issa_wrap_spec.rb
@@ -3,7 +3,26 @@ RSpec.describe IssaWrap do
     expect(IssaWrap::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "returns an empty string" do
+    expect(IssaWrap::Wrapper.wrap(nil,5)).to eq("")
+    expect(IssaWrap::Wrapper.wrap("",5)).to eq("")
   end
+
+  it "returns string if string is shorter than column" do
+    expect(IssaWrap::Wrapper.wrap("word",4)).to eq("word")
+    expect(IssaWrap::Wrapper.wrap("word",7)).to eq("word")
+    expect(IssaWrap::Wrapper.wrap("some words",12)).to eq("some words")
+  end
+
+  it "returns breaks string on column" do
+    expect(IssaWrap::Wrapper.wrap("longword",4)).to eq("long\nword")
+    expect(IssaWrap::Wrapper.wrap("verylongword",4)).to eq("very\nlong\nword")
+    expect(IssaWrap::Wrapper.wrap("longishword",7)).to eq("longish\nword")
+  end
+
+  it "breaks at spaces where possible" do
+    expect(IssaWrap::Wrapper.wrap("long word",4)).to eq("long\nword")
+    expect(IssaWrap::Wrapper.wrap("very long word",4)).to eq("very\nlong\nword")
+  end
+
 end
